7664e7f71ccbe735f20f6afbb5ad62497db5fd92
TWLord <tom.lord@warwick.ac.uk>
2020-05-12 19:50:21 +0100

    tweaks to SLURM jobsub scripts

--- jobsub/SLURM/pymovedata_dojobsub.sh
+++ jobsub/SLURM/pymovedata_dojobsub.sh
@@ -6,7 +6,11 @@ MAUSdir=/storage/epp2/phumhf/MICE/maus--versions/MAUSv3.3.2
 DATApath=/storage/epp2/mice/phumhf
 #DATApath=/data/mice/phumhf
 
-time="96:00:00"
+#TIME="96:00:00"
+#TIME="5760"
+#TIME="34"
+#TIME="5:00:00:00"
+TIME="UNLIMITED"
 
 ABS="$1"
 rn="$2"
@@ -84,7 +88,7 @@ echo \" ---- Copying root files for analysis ---- \"
 echo cp $datadir/*.root \$mytempdir/
 cp $datadir/*.root \$mytempdir/
 
-sed -i \"s%.*a_file = .*%        a_file = \\\"\$mytempdir\/\\*_sim.root\\\"%\" $here/$configdir/config_${config}_${rn}_full.py
+sed -i \"s%.*a_file = .*%        a_file = \\\"\$mytempdir\/\\*.root\\\"%\" $here/$configdir/config_${config}_${rn}_full.py
 
 echo \"Copied files : \"
 echo \"\$(ls)\"
@@ -100,5 +104,6 @@ python $here/bin/run_one_analysis.py $configdir/config_${config}_${rn}_full.py
 chmod +x $here/logs/tmp/${runnumber}_${jobsuffix}_movedata.sh
 #bsub -G micegrp -M 20000 -oo $here/logs/${runnumber}_${jobsuffix}_movedata.log -q ${queue} $here/logs/tmp/${runnumber}_${jobsuffix}_movedata.sh
 
-sbatch -o $here/logs/${runnumber}_${jobsuffix}_movedata.log -t ${time} $here/logs/tmp/${runnumber}_${jobsuffix}_movedata.sh
+echo "sbatch -o $here/logs/${runnumber}_${jobsuffix}_movedata.log -p epp -t ${TIME} $here/logs/tmp/${runnumber}_${jobsuffix}_movedata.sh"
+sbatch -o $here/logs/${runnumber}_${jobsuffix}_movedata.log -p epp -t ${TIME} $here/logs/tmp/${runnumber}_${jobsuffix}_movedata.sh
 
