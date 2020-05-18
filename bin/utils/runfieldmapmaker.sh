54eac9455fdaf7fe3d09f54c084aba36e861e432
TWLord <tom.lord@warwick.ac.uk>
2020-05-18 18:53:59 +0100

    Updated branch with stuff / junk from current version

--- bin/utils/runfieldmapmaker.sh
+++ bin/utils/runfieldmapmaker.sh
@@ -4,7 +4,8 @@ cd /home/phumhf/MICE/maus--versions/MAUSv3.3.0/bin/user/first-observation-paper-
 . ../../../env.sh
 cd bin/utils
 
-for rn in 9883 9885 9886 10243 10245 10246 10313 10315 10318 10319 10508 10504 10509 ; do 
+#for rn in 9883 9885 9886 10243 10245 10246 10313 10315 10318 10319 10508 10504 10509 ; do 
+for rn in 9909 9910 9911 10265 10267 10268 ; do 
 
 if [ $rn -lt 10000 ] ; then
  runnumber=0$rn
@@ -14,8 +15,8 @@ fi
 
 mkdir -p ./fields_$rn
 
-#python make_field_map.py --simulation_geometry_filename /data/mice/phumhf/Geometries/runnumber_$runnumber/ParentGeometryFile.dat
-python make_field_map.py --simulation_geometry_filename /data/mice/phumhf/ReconData/Mausv3.3.2/$runnumber/geo-$runnumber/ParentGeometryFile.dat
+python make_field_map.py --simulation_geometry_filename /data/mice/phumhf/Geometries/runnumber_$runnumber/ParentGeometryFile.dat
+#python make_field_map.py --simulation_geometry_filename /data/mice/phumhf/ReconData/Mausv3.3.2/$runnumber/geo-$runnumber/ParentGeometryFile.dat
 
 
 mv field/ fields_$rn/
